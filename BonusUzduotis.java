fddb019687dd7a64698a902ee386e7ec1bfe7231
Edgaras Steponaitis <91746111+Laysiakas@users.noreply.github.com>
2025-02-18 15:05:41 +0200

    Saved local changes for BonusUzduotis.java

--- BonusUzduotis.java
+++ BonusUzduotis.java
@@ -1,27 +1,30 @@
 public class BonusUzduotis {
     public static String compress(String input) {
+        // if input string is empty, return empty string.
         if (input.isEmpty()) {
             return "";
         }
         StringBuilder compressed = new StringBuilder();
         int count = 1;
-
+        // input.length() - 1 because we are comparing current character with the next one.
         for (int i = 1; i < input.length(); i++) {
             if (input.charAt(i) == input.charAt(i - 1)) {
                 count++;
+                // if we are at the end of the string, append the last character and count.
             } else {
                 compressed.append(input.charAt(i - 1));
                 if (count > 1) {
                     compressed.append(count);
                 }
                 count = 1;
+            
             }
         }
-        compressed.append(input.charAt(input.length() - 1));
+        compressed.append(input.charAt(input.length() - 1)); 
 
-        return compressed.toString();
+        return compressed.toString(); // return compressed string.
     }
-    public static String decompress(String input) {
+    public static String decompress(String input) { 
         if (input.isEmpty()) {
             return "";
         }
@@ -31,18 +34,18 @@ public class BonusUzduotis {
 
         for (int i = 1; i < input.length(); i++) {
             if (Character.isDigit(input.charAt(i))) {
-                count = count * 10 + Character.getNumericValue(input.charAt(i));
+                count = count * 10 + Character.getNumericValue(input.charAt(i)); // get the number of repetitions.
             } else {
                 for (int j = 0; j < count; j++) {
                     decompressed.append(currentChar);
-                }
+                } // append the character count times.
                 currentChar = input.charAt(i);
                 count = 0;
             }
         }
         for (int j = 0; j < count; j++) {
             decompressed.append(currentChar);
-        }
+        } // append the last character count times.
 
         return decompressed.toString();
     }
